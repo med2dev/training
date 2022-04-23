823505af3fdf2c79175d363b17c43dbdcaa4e57b
hatdeck <daniel.bryce.hall@gmail.com>
2022-04-22 17:48:56 -0700

    async example

---
+++ ex2022-04-22/async.js
@@ -0,0 +1,7 @@
+async function getACuteDogPhoto(){
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await res. json()
+  console.log(data)
+}
+
+getACuteDogPhoto()
