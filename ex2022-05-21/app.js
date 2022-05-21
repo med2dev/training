ae249aa7efe4269bc68fbc072241455f9880583d
hatdeck <daniel.bryce.hall@gmail.com>
2022-05-21 16:56:45 -0700

    documentation

--- ex2022-05-21/app.js
+++ ex2022-05-21/app.js
@@ -1,8 +1,12 @@
 fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
+    .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
     })
     .catch(err => {
-        console.log(`error ${err}`)
+        console.log(`error ${err}`) 
     })
+
+// fetch is a web API. It returns a promise. 
+// Why? because JS can't wait. It needs a promise. 
+// It is a synchronous single threaded language that waits for no one
