2b0200fc532fa0cc304673b1b6f7506692347889
hatdeck <daniel.bryce.hall@gmail.com>
2022-05-21 17:01:31 -0700

    May 21 exercise 2

---
+++ ex2022-05-21/app2.js
@@ -0,0 +1,31 @@
+function houseOne(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve('Paper delivered to house 1')
+        , 1000})
+    })
+}
+
+function houseTwo(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 2')
+        , 2000})
+    })
+}
+
+function houseThree(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 3')
+        , 3000})
+    })
+}
+
+houseOne()
+    .then(data => console.log(data))
+    .then(houseTwo)
+    .then(data => console.log(data))
+    .then(houseThree)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
